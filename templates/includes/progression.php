7ef2a2d6d0a549edd80630105082ef6a9886f0ab
YoLoN <shadyolown13@onet.pl>
2021-04-18 22:16:06 +0200

    add basic structure to progression bar

--- templates/includes/progression.php
+++ templates/includes/progression.php
@@ -1,3 +1,14 @@
 <section class="progression-content">
-    
+    <div class="date-wrapper">
+        <div class="day">18</div>
+        <div class="month">APR</div>
+    </div>
+    <div class="content-wrapper">
+        <h2 class="headline h2">Today's Tasks</h2>
+        <div class="progression-bar"><div class="progressed"></div></div>
+        <div class="additional-data-wrapper">
+            <div class="done">Done Todo: 0</div>
+            <div class="todo">Must do: 0</div>
+        </div>
+    </div>
 </section>
